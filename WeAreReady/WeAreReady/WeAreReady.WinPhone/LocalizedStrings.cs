055ddb17cc03a2fb34229a1c116a89abaceb89bf
hieumoscow89 <hieumoscow89@yahoo.com>
2014-08-19 17:37:06 +0700

    First Commit

---
+++ WeAreReady/WeAreReady/WeAreReady.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using WeAreReady.WinPhone.Resources;
+
+namespace WeAreReady.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
